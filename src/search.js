576d7658fa13a00b5c78e5d9e217d902458cde8a
tom88901 <chinguyenvan2k.08@gmail.com>
2025-08-02 11:34:39 +0700

    Update search.js

--- src/search.js
+++ src/search.js
@@ -1,24 +1,29 @@
 load('config.js');
-function execute(key,page) {
-    if(!page) page = '1';
-    let response = fetch("https://api.uaa.com/novel/app/novel/search?keyword=" + key + "&orderType=1&page=" + page + "&size=10");
-    if (response.ok) {
-        let json = response.json();
-        let ele = json.model.data
-        let books = [];
-        ele.forEach(e => 
-        {
-            books.push({
-                name: e.title,
-                link: "https://www.uaa.com/api/novel/app/novel/intro?id=" + e.id,
-                cover: e.coverUrl,
-                description: e.authors,
-                host: BASE_URL
-            })
-        });
-        let next = (parseInt(page) + 1).toString();
-        return Response.success(books,next);
-    }
 
+function execute(key, page) {
+    if (!page) page = '1';
+
+    // Tạo URL tìm kiếm mới
+    let url = BASE_URL + '/novel/list?searchType=1&sort=2&page=' + page + '&keyword=' + key;
+    let response = fetch(url);
+
+    if (response.ok) {
+        let doc = response.html();
+        const data = [];
+
+        // Thay đổi bộ chọn (selector) để lấy đúng thông tin
+        doc.select(".main_box .novel_list_box ul li").forEach(e => {
+            data.push({
+                name: e.select(".title").text(),
+                link: e.select(".cover_box a").attr('href'),
+                cover: e.select(".cover_box a img").attr('src'),
+                description: e.select(".info_box a").first().text(),
+                host: BASE_URL
+            });
+        });
+
+        let next = (parseInt(page) + 1).toString();
+        return Response.success(data, next);
+    }
     return null;
 }
