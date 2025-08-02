529883c99ba5f7ec8330287b5f33280b3e05fcca
tom88901 <chinguyenvan2k.08@gmail.com>
2025-08-02 13:20:52 +0700

    Update detail.js

--- uaa/src/detail.js
+++ uaa/src/detail.js
@@ -1,26 +1,18 @@
 function execute(url) {
-    let response = fetch(url);
+    let response = fetch(url)
     if (response.ok) {
         let doc = response.html();
-
-        // Vùng chứa chung
-        let book = doc.select(".main_box .content_box .left_box");
-
-        // Lấy phần "Thông tin" chi tiết
-        let detail = "";
-        book.select(".info_box div").forEach(e => {
-            detail += e.text() + "<br>";
-        });
-        
-        // Lấy riêng phần "Giới thiệu" và làm sạch nó
-        let descriptionElement = book.select(".brief_box").clone();
-        descriptionElement.select("a").remove(); // Xóa thẻ a chứa chữ "开始阅读"
-
+        console.log(doc)
+        let book = doc.select(".main_box .content_box .left_box")
+        let detail="";
+        book.select(".info_box div").forEach( e=>{
+                detail+=e.text()+"<br>"
+        })
         return Response.success({
             name: book.select(".info_box h1").text(),
             cover: book.select(".cover").attr('src'),
-            author: book.select(".info_box div a").first().text(),
-            description: descriptionElement.text().replace(/\r?\n/g,"<br>"),
+            author: book.select(".info_box div").get(2).text().replace("作者：", ""),
+            description: book.select(".brief_box").text().replace(/\r?\n/g,"<br>"),
             detail: detail,
             host: "https://www.uaa.com"
         });
